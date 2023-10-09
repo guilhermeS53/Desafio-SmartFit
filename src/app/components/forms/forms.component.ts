43ff96f272e7a43222df560215077573951f203a
Guilherme Sousa <guilherme.gs17@outlook.com>
2023-10-09 15:18:45 -0300

    fix: problem in ngOnInit

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,18 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
-  styleUrls: ['./forms.component.scss']
+  styleUrls: ['./forms.component.scss'],
 })
 export class FormsComponent {
-
   results = [];
+  formGroup!: FormGroup;
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {}
 
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false
+    })
   }
-  ngonInit(): void {
-    
+
+  onSubmit(): void {
+    console.log(this.formGroup.value);
+  }
+
+  onClean(): void {
+    this.formGroup.reset();
   }
 }
